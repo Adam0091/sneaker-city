d5360560fc011d4d630685bc489473eb3bb2de74
Andrey <adam.holo@mail.ru>
2022-08-14 18:45:51 +0400

    fix: change rules eslint and change index.html

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'indent': 'off',
+    'quotes': 'off',
+    'quote-props': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
