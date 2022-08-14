4875a94d29ae3284ee683eced1444b4d3a9aa487
Andrey <adam.holo@mail.ru>
2022-08-14 18:53:32 +0400

    feat: add HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
+
+import AppHeader from '@/components/AppHeader.vue'
+import AsideBar from '@/components/AsideBar.vue'
+import AppFooter from '@/components/AppFooter.vue'
+import ProductsList from '@/components/ProductsList.vue'
+
+import { GET_PRODUCTS, GET_PRODUCTS_BY_CATEGORY } from '@/utils/network'
+
+import { ProductType } from '@/types/index'
+
+const products = ref<ProductType[]>([])
+const filterProducts = ref<ProductType[]>([])
+const contegory = ref('')
+const loading = ref(true)
+
+onMounted(async () => {
+  products.value = await GET_PRODUCTS()
+  filterProducts.value = [...products.value]
+  loading.value = false
+})
+
+watch(contegory, async () => {
+  loading.value = true
+  products.value = await GET_PRODUCTS_BY_CATEGORY(contegory.value)
+  filterProducts.value = [...products.value]
+  loading.value = false
+})
+
+const filter = (filterOptions: any) => {
+  filterProducts.value = products.value.filter(product =>
+    product.price >= filterOptions.value.price.min &&
+    product.price <= filterOptions.value.price.max &&
+    product.rating.rate >= filterOptions.value.rating
+  )
+}
+</script>
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <AppHeader @change-category="contegory = $event" />
+  <div class="content">
+    <AsideBar @change-filter="filter" />
+    <div class="wrapper">
+      <main class="main">
+        <ProductsList :products="filterProducts" :loading="loading" />
+      </main>
+      <AppFooter />
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<style lang="scss" scoped>
+.content {
+  display: flex;
+}
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-})
-</script>
+.main {
+  padding: 32px 0 92px 0;
+  min-height: 800px;
+}
+
+.wrapper {
+  margin: 0 64px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+</style>
