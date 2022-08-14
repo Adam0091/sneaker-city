38eb1a114fb90c9c977bb19402e4b79b0f20c121
Andrey <adam.holo@mail.ru>
2022-08-14 22:59:16 +0400

    fix: add constants

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,12 +1,18 @@
+import { PRODUCTS_URL, CATEGORIES_URL, CATEGORY_URL } from '@/constants/index'
+import { categoriesEnums } from '@/types/enums'
+
 export const GET_PRODUCTS = async () =>
-  await fetch('https://fakestoreapi.com/products').then((res) => res.json())
+  await fetch(PRODUCTS_URL).then((res) => res.json())
+
+export const GET_CATEGORIES = async () =>
+  await fetch(CATEGORIES_URL).then((res) => res.json())
 
 export const GET_PRODUCTS_BY_CATEGORY = async (category: string) => {
-  if (category === 'all') {
+  if (category === categoriesEnums.All) {
     return GET_PRODUCTS()
   } else {
     return await fetch(
-      `https://fakestoreapi.com/products/category/${category}`
+      `${CATEGORY_URL}/${category}`
     ).then((res) => res.json())
   }
 }
