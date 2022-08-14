a0da7027cf03423dc760a2dd07a351bcec36ceeb
Andrey <adam.holo@mail.ru>
2022-08-14 22:59:53 +0400

    fix: eslint and add a:tel

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,23 +1,23 @@
 <template>
-    <footer class="footer">
-        <div class="footer__column">
-            <div class="footer__social-networks">
-                <span>Don’t missout on once-in-a-while-deals:</span>
-                <ul class="social-networks__list">
-                    <li class="social-networks__item" v-for="socialNetwork in socialNetworks" :key="socialNetwork.name">
-                        <img :src="socialNetwork.img" :alt="socialNetwork.name" />
-                    </li>
-                </ul>
-            </div>
-            <div class="footer__telephon">
-                <span>Support line: </span>
-                <span>+250 788 467 808</span>
-            </div>
-        </div>
-        <div class="footer__column footer__license">
-            <span>Copyright 2021 © Sneaker City ltd</span>
-        </div>
-    </footer>
+  <footer class="footer">
+    <div class="footer__column">
+      <div class="footer__social-networks">
+        <span>Don’t missout on once-in-a-while-deals:</span>
+        <ul class="social-networks__list">
+          <li class="social-networks__item" v-for="socialNetwork in socialNetworks" :key="socialNetwork.name">
+            <img :src="socialNetwork.img" :alt="socialNetwork.name" />
+          </li>
+        </ul>
+      </div>
+      <div class="footer__telephon">
+        <span>Support line: </span>
+        <a href="tel:+250 788 467 808">+250 788 467 808</a>
+      </div>
+    </div>
+    <div class="footer__column footer__license">
+      <span>Copyright 2021 © Sneaker City ltd</span>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -39,49 +39,63 @@ const socialNetworks = [
 
 <style lang="scss" scoped>
 .footer {
-    justify-self: flex-end;
-    width: 100%;
+  justify-self: flex-end;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  border-top: 1px solid rgba(0, 0, 0, 0.15);
+  padding: 34px 0 64px 0;
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+
+  color: rgba(0, 0, 0, 0.65);
+
+  &__social-networks {
     display: flex;
-    justify-content: space-between;
+    column-gap: 12px;
+  }
 
-    border-top: 1px solid rgba(0, 0, 0, 0.15);
-    padding: 34px 0 64px 0;
-
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 24px;
-
-    color: rgba(0, 0, 0, 0.65);
-
-    &__social-networks {
-        display: flex;
-        column-gap: 12px;
+  &__telephon {
+    a {
+      color: rgba(0, 0, 0, 0.65);
+      text-decoration: none;
     }
-    &__license {
-        display: flex;
-        align-items: flex-end;
-    }
+  }
+
+  &__license {
+    display: flex;
+    align-items: flex-end;
+  }
 }
 
 .social-networks {
 
-    &__list {
-        display: flex;
-        align-items: center;
-        list-style: none;
-        column-gap: 12px;
-    }
+  &__list {
+    display: flex;
+    align-items: center;
+    list-style: none;
+    column-gap: 12px;
+  }
 
-    &__item {
-        width: 32px;
-        height: 32px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  &__item {
+    width: 32px;
+    height: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-        background: rgba(0, 0, 0, 0.04);
-        border-radius: 8px;
-        cursor: pointer;
+    background: rgba(0, 0, 0, 0.04);
+    border-radius: 8px;
+    cursor: pointer;
+
+    transition: all 0.2s;
+
+    &:hover {
+      background: rgba(19, 17, 17, 0.168);
     }
+  }
 }
 </style>
