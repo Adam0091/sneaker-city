7e45053ba3d3d4025aa93e2886b0aa7278946c5e
Andrey <adam.holo@mail.ru>
2022-08-14 22:58:50 +0400

    feat: add FilterOptionsType

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,17 @@ export type ProductType = {
   id: string
   title: string
   image: string
-  price: string
+  price: number
   rating: {
     rate: number
     count: number
   }
+}
+
+export type FilterOptionsType = {
+  price: {
+    min: number,
+    max: number
+  },
+  rating: number
 }
