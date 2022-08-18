025b654b12cd54e8e087ee377b09ca0cacd536f8
Andrey <adam.holo@mail.ru>
2022-08-18 21:14:40 +0400

    feat: add BasketView

---
+++ src/views/BasketView.vue
@@ -0,0 +1,32 @@
+<template>
+  <AppHeader />
+
+  <main class="main">
+    <ProductBasket />
+    <AppFooter />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue"
+
+import ProductBasket from "@/components/ProductBasket.vue"
+import AppHeader from "@/components/AppHeader.vue"
+import AppFooter from "@/components/AppFooter.vue"
+
+import { useBasketStore } from "@/stores/basketStore"
+import { ProductType } from "@/types"
+
+const products = ref<ProductType[]>([])
+
+onMounted(() => {
+  const store = useBasketStore()
+  products.value = store.basket
+})
+</script>
+
+<style scoped>
+.main {
+  margin: 0 64px;
+}
+</style>
