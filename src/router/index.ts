a0230cde2f7ca16c97245de4b12c7ac9c9b8958e
Andrey <adam.holo@mail.ru>
2022-08-18 21:10:43 +0400

    feat: add new router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import BasketView from '@/views/BasketView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
     component: HomeView
+  },
+  {
+    path: '/:category',
+    component: HomeView
+  },
+  {
+    path: '/basket',
+    component: BasketView
   }
 ]
 
