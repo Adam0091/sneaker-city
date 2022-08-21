2b4490f466628d503c52d3e224b2a2c5e7c00f32
Andrey <adam.holo@mail.ru>
2022-08-21 17:23:45 +0400

    fix: rename component

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -1,73 +1,73 @@
 <template>
-  <div class="product-model" @click.stop="$emit('clickModel')">
-    <div class="product-model__container" @click.stop>
-      <div class="product-model__product">
-        <div class="product__header">
-          <div class="product__description">
-            <div class="product__name">
-              <h5>{{ product.title }}</h5>
-            </div>
-            <div class="product__price">
-              <span>{{ product.price }}RWF</span>
-            </div>
-          </div>
-
-          <div
-            class="product__add-favorite"
-            :class="{ 'product__add-favorite--active': isFavoriteProduct }"
-            @click="handleFavorite"
-          >
-            <svg
-              width="24"
-              height="22"
-              viewBox="0 0 24 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M20.8401 3.60999C20.3294 3.099 19.7229 2.69364 19.0555 2.41708C18.388 2.14052 17.6726 1.99817 16.9501 1.99817C16.2276 1.99817 15.5122 2.14052 14.8448 2.41708C14.1773 2.69364 13.5709 3.099 13.0601 3.60999L12.0001 4.66999L10.9401 3.60999C9.90843 2.5783 8.50915 1.9987 7.05012 1.9987C5.59109 1.9987 4.19181 2.5783 3.16012 3.60999C2.12843 4.64169 1.54883 6.04096 1.54883 7.49999C1.54883 8.95903 2.12843 10.3583 3.16012 11.39L4.22012 12.45L12.0001 20.23L19.7801 12.45L20.8401 11.39C21.3511 10.8792 21.7565 10.2728 22.033 9.60535C22.3096 8.93789 22.4519 8.22248 22.4519 7.49999C22.4519 6.77751 22.3096 6.0621 22.033 5.39464C21.7565 4.72718 21.3511 4.12075 20.8401 3.60999V3.60999Z"
-                stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </div>
-        </div>
-
-        <div class="product__slider-images">
-          <UiSliderImage :images="fakeArrayImages" />
-        </div>
-      </div>
-
-      <div class="product-model__description">
-        <div class="description__header">
-          <div class="description__title-wrapper">
-            <div class="description__title">
-              <span>Description</span>
+  <Transition name="modal">
+    <div v-if="show" class="product-modal" @click="$emit('close')">
+      <div class="product-modal__container" @click.stop>
+        <div class="product-modal__product">
+          <div class="product__header">
+            <div class="product__description">
+              <div class="product__name">
+                <h5>{{ product.title }}</h5>
+              </div>
+              <div class="product__price">
+                <span>{{ product.price }}RWF</span>
+              </div>
             </div>
 
             <div
-              class="description__arrow"
-              :class="{ 'description__arrow--active': isHiddenDescription }"
-              @click="handleArrow"
+              class="product__add-favorite"
+              :class="{ 'product__add-favorite--active': isFavoriteProduct }"
+              @click="handleFavorite"
             >
-              <img src="@/assets/images/arrowUp.png" alt="show or hidden description" />
+              <svg
+                width="24"
+                height="22"
+                viewBox="0 0 24 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M20.8401 3.60999C20.3294 3.099 19.7229 2.69364 19.0555 2.41708C18.388 2.14052 17.6726 1.99817 16.9501 1.99817C16.2276 1.99817 15.5122 2.14052 14.8448 2.41708C14.1773 2.69364 13.5709 3.099 13.0601 3.60999L12.0001 4.66999L10.9401 3.60999C9.90843 2.5783 8.50915 1.9987 7.05012 1.9987C5.59109 1.9987 4.19181 2.5783 3.16012 3.60999C2.12843 4.64169 1.54883 6.04096 1.54883 7.49999C1.54883 8.95903 2.12843 10.3583 3.16012 11.39L4.22012 12.45L12.0001 20.23L19.7801 12.45L20.8401 11.39C21.3511 10.8792 21.7565 10.2728 22.033 9.60535C22.3096 8.93789 22.4519 8.22248 22.4519 7.49999C22.4519 6.77751 22.3096 6.0621 22.033 5.39464C21.7565 4.72718 21.3511 4.12075 20.8401 3.60999V3.60999Z"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
             </div>
           </div>
 
-          <div
-            class="description__container"
-            :class="{ 'description__container--hidden': isHiddenDescription }"
-          >
-            <p class="description__text">
-              {{ product.description }}
-            </p>
+          <div class="product__slider-images">
+            <UiSliderImage :images="fakeArrayImages" />
           </div>
         </div>
 
-        <div class="product-model__add-to-card">
-          <template v-if="!isProductInBasket">
+        <div class="product-modal__description">
+          <div class="description__header">
+            <div class="description__title-wrapper">
+              <div class="description__title">
+                <span>Description</span>
+              </div>
+
+              <div
+                class="description__arrow"
+                :class="{ 'description__arrow--active': isHiddenDescription }"
+                @click="handleArrow"
+              >
+                <img src="@/assets/images/arrowUp.png" alt="show or hidden description" />
+              </div>
+            </div>
+
+            <div
+              class="description__container"
+              :class="{ 'description__container--hidden': isHiddenDescription }"
+            >
+              <p class="description__text">
+                {{ product.description }}
+              </p>
+            </div>
+          </div>
+
+          <div class="product-modal__add-to-card">
             <UiCounter
               :count="amountProduct"
               @updateCount="updateAmountProduct"
@@ -80,24 +80,15 @@
               width="135px"
               :disabled="false"
             />
-          </template>
-
-          <template v-else>
-            <UiButton
-              @click="handleDeleteProduct"
-              text="Удалить товар из корзины"
-              width="250px"
-              :disabled="false"
-            />
-          </template>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
-import { defineProps, onMounted, ref, toRefs, watch } from "vue"
+import { defineProps, ref, toRefs } from "vue"
 
 import { useFavoriteStore } from "@/stores/favoritesStore"
 
@@ -110,41 +101,43 @@ import { useBasketStore } from "@/stores/basketStore"
 
 type TProps = {
   product: ProductType;
+  show: boolean;
 };
 
 const props = defineProps<TProps>()
-const { product } = toRefs(props)
+const { product, show } = toRefs(props)
 
 const basketStore = useBasketStore()
 const favoriteStore = useFavoriteStore()
 
+const amountProductInBasket = () => {
+  const basketProduct = basketStore.basket.filter((item) => item.id === product.value.id)
+  if (basketProduct.length !== 0) {
+    return basketProduct[0].amount
+  }
+
+  return 1
+}
+
 const isHiddenDescription = ref(false)
 const isFavoriteProduct = ref(false)
-const amountProduct = ref(1)
+const amountProduct = ref(amountProductInBasket())
 
 const fakeArrayImages = Array(4).fill(product.value.image)
 
-onMounted(() => {
-  const favoriteProducts = favoriteStore.favoritesProduct
-  if (favoriteProducts.filter((item) => item.id === product.value.id).length !== 0) {
-    isFavoriteProduct.value = true
-  }
-})
-
-watch(basketStore, () => {
-  isProductInBasket.value =
+const handleAddProduct = () => {
+  const isProductInBasket =
     basketStore.basket.filter((item) => item.id === product.value.id).length !== 0
-})
 
-const isProductInBasket = ref(
-  basketStore.basket.filter((item) => item.id === product.value.id).length !== 0
-)
+  if (isProductInBasket) {
+    basketStore.changeAmount(product.value.id, amountProduct.value)
+  } else {
+    basketStore.addBasketProduct(
+      Object.assign({ amount: amountProduct.value }, product.value)
+    )
+  }
+}
 
-const handleAddProduct = () =>
-  basketStore.addBasketProduct(
-    Object.assign({ amount: amountProduct.value }, product.value)
-  )
-const handleDeleteProduct = () => basketStore.removeBasketProduct(product.value.id)
 const handleArrow = () => (isHiddenDescription.value = !isHiddenDescription.value)
 const handleFavorite = () => {
   if (isFavoriteProduct.value) {
@@ -162,7 +155,7 @@ const updateAmountProduct = (value: number) => {
 </script>
 
 <style lang="scss" scoped>
-.product-model {
+.product-modal {
   position: fixed;
   top: 0;
   bottom: 0;
@@ -333,5 +326,19 @@ const updateAmountProduct = (value: number) => {
 
     color: rgba(0, 0, 0, 0.65);
   }
+}
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
